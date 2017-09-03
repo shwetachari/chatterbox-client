ae497b56844f3e505a5d4eabb69fe540e3b2a5dc
shwetachari <shwetachari@gmail.com>
2017-09-02 17:12:39 -0700

    condensed fetch method function body and added styling

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,8 @@
 // YOUR CODE HERE:
-var app = {
-  server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-};
-app.init = function() {
+var app = {};
 
+app.init = function() {
+  app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 };
 
 app.send = function(message) {
@@ -23,67 +22,37 @@ app.send = function(message) {
   });
 };
 
-app.fetch = function(room) {
-  if (room === undefined) {
-    console.log('fetching when room is undefined');
-    $.ajax({
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-      type: 'GET',
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
-      data: jQuery.param({
-        "order": "-createdAt"
-      }),
-      success: function(data) {
-        console.log('data', data);
-        var rooms = {};
-        for (let i = data.results.length - 1; i >= 0; i--) {
-          if (data.results[i].username !== null && data.results[i].username !== '' && data.results[i].username !== undefined) {
-            app.renderMessage(data.results[i]);
-            if (!rooms.hasOwnProperty(data.results[i].roomname)) {
-              rooms[data.results[i].roomname] = data.results[i].roomname;
-              app.renderRoom(data.results[i].roomname);
-            }
+app.fetch = function(friends, room) {
+  var dataQuery = room === undefined ? { "order": "-createdAt" } : { "order": "-createdAt", "where": {'roomname' : room}};
+  $.ajax({
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+    data: jQuery.param(dataQuery),
+    success: function(data) {
+      console.log('data', data);
+      app.clearMessages();
+      var rooms = {};
+      for (let i = data.results.length - 1; i >= 0; i--) {
+        if (data.results[i].username !== null && data.results[i].username !== '' && data.results[i].username !== undefined) {
+          app.renderMessage(data.results[i]);
+          if (!rooms.hasOwnProperty(data.results[i].roomname) && data.results[i].roomname !== '' && data.results[i].roomname !== null && data.results[i].roomname !== undefined) {
+            rooms[data.results[i].roomname] = data.results[i].roomname;
+            app.renderRoom(data.results[i].roomname);
           }
         }
-      },
-      error: function(data) {
-        console.log('error in fetch');
       }
-    });
-  } else {
-    console.log('fetching when room is defined');
-    $.ajax({
-      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-      type: 'GET',
-      dataType: 'json',
-      contentType: 'application/json; charset=utf-8',
-      data: jQuery.param({
-        "order": "-createdAt",
-        "where": {'roomname' : room}
-      }),
-      success: function(data) {
-        console.log('data in success of else', data);
-        var rooms = {};
-        for (let i = data.results.length - 1; i >= 0; i--) {
-          if (data.results[i].username !== null && data.results[i].username !== '' && data.results[i].username !== undefined) {
-            app.renderMessage(data.results[i]);
-            if (!rooms.hasOwnProperty(data.results[i].roomname)) {
-              rooms[data.results[i].roomname] = data.results[i].roomname;
-              app.renderRoom(data.results[i].roomname);
-            }
-          }
-        }
-      },
-      error: function(data) {
-        console.log('error');
-      }
-    });
-  }
-  
+      app.handleUsernameClick(friends);
+    },
+    error: function(data) {
+      console.log('error in fetch');
+    }
+  });
 };
 
 app.clearMessages = function() {
+  $('#roomSelect ul').html('');
   $('#chats').html('');
 };
 
@@ -92,15 +61,17 @@ app.renderMessage = function(obj) {
   $username.text(obj.username);
   var $text = $('<div class="text"><div>');
   $text.text(obj.text);
+  var $time = $('<div class="timestamp"></div>');
+  var formattedTime = (obj.createdAt.split('').slice(11, 16).join(''));
+  $time.text(formattedTime);
   var $chat = $('<div class="chat"></div>');
-  $chat.append($username, $text);
+  $chat.append($username, $text, $time);
   $chat.addClass(obj.roomname);
   $('#chats').prepend($chat);
 };
 
 app.renderRoom = function(roomName) {
   var $room = $('<li></li>');
-  // $room.html("<a href='#'>" + roomName + "</a>");
   $room.text(roomName);
   $('#roomSelect ul').append($room);
 };
@@ -109,7 +80,7 @@ app.handleUsernameClick = function(array) {
   $('.chat .username').each(function(index, user) {
     for (var i = 0; i < array.length; i++) {
       if ($(user).text() === array[i]) {
-        $(user).css('color', 'skyblue');
+        $(user).css('color', 'black');
       }
     }
   });
@@ -124,46 +95,37 @@ app.handleSubmit = function(room) {
   app.send(obj);
   $('#inputUsername').val('');
   $('#inputMessage').val('');
-  $('#roomSelect ul').html('');
-  app.clearMessages();
-  app.fetch(room);
+  app.fetch(friends, room);
 };
 
 $(document).ready(function() {
+  app.init();
+  
   var friends = [];
-  var globalRoom;
+  var globalRoom = 'lobby';
   var $roomName = $('<div></div>');
   
-  $roomName.text(globalRoom);
+  $roomName.text('You are currently in "' + globalRoom + '"');
   $('#main').append($roomName);
-  app.clearMessages();
-  app.fetch(globalRoom);
-  app.handleUsernameClick(friends);
+  app.fetch(friends, globalRoom);
+  
   var intervalOn = true;
   var interval = setInterval(() => {
     if (intervalOn) {
-      $('#roomSelect ul').html('');
-      app.clearMessages();
-      app.fetch();
-      app.handleUsernameClick(friends);
+      app.fetch(friends);
     }
   }, 10000);
   
   $('#roomSelect ul').on('click', 'li', function() {
     var selectedRoom = $(this).text();
     globalRoom = selectedRoom;
-    $roomName.text(globalRoom);
-    app.clearMessages();
-    app.fetch(globalRoom);
-    app.handleUsernameClick(friends);
+    $roomName.text('You are currently in "' + globalRoom + '"');
+    app.fetch(friends, globalRoom);
     intervalOn = false;
   });
   
   $('#roomSelect button').click(function() {
-    $('#roomSelect ul').html('');
-    app.clearMessages();
-    app.fetch();
-    app.handleUsernameClick(friends);
+    app.fetch(friends);
     intervalOn = true;
   });
 
